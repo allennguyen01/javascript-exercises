80c075eb0763b36288d1f6c3b8efc41364dca91e
Allen Nguyen <anhn321@gmail.com>
2022-11-22 00:42:50 -0800

    Complete and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...theArgs) {
+	do {
+		let elemToRemove = theArgs.shift();
+		let indexToRemove = array.indexOf(elemToRemove);
+		if (indexToRemove !== -1) {
+			array.splice(indexToRemove, 1);
+		}
+	} while (theArgs.length !== 0);
 
+	return array;
 };
 
 // Do not edit below this line
