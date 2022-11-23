f1a149ec0eaab2735e96ba736493acdb309d9c71
Allen Nguyen <anhn321@gmail.com>
2022-11-22 22:44:55 -0800

    Complete and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (firstInt, secondInt) {
+	if (
+		firstInt < 0 ||
+		secondInt < 0 ||
+		typeof firstInt !== "number" ||
+		typeof secondInt !== "number"
+	) {
+		return "ERROR";
+	}
 
+	let sum = 0;
+
+	if (firstInt > secondInt) {
+		let temp = firstInt;
+		firstInt = secondInt;
+		secondInt = temp;
+	}
+
+	for (let i = firstInt; i <= secondInt; i++) {
+		sum += i;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
