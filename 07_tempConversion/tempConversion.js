dc8d8ebe9849300bec5fec5000fae2ecb345de48
Allen Nguyen <anhn321@gmail.com>
2022-11-23 23:09:37 -0800

    Complete and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+	let celsuis = (5 / 9) * (fTemp - 32);
+	let celsuisRounded = Math.round(celsuis * 10) / 10;
+	return celsuisRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsuis) {
+	let fahrenheit = (9 / 5) * celsuis + 32;
+	let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+	return fahrenheitRounded;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
