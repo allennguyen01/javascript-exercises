e2f3ac41a13706a514dbbdd441585ec419a5217c
Allen Nguyen <anhn321@gmail.com>
2022-11-30 21:27:56 -0800

    Complete and passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	let newString = string
+		.toLowerCase()
+		.split("")
+		.filter((letter) => /^[a-zA-Z]+$/.test(letter))
+		.join("");
 
+	console.log(newString);
+
+	let reversedString = string
+		.toLowerCase()
+		.split("")
+		.filter((letter) => /^[a-zA-Z]+$/.test(letter))
+		.reverse()
+		.join("");
+	return reversedString === newString;
 };
+
+// console.log(palindromes("A car, a man, a maraca."));
+console.log(palindromes("tacos"));
 
 // Do not edit below this line
 module.exports = palindromes;
